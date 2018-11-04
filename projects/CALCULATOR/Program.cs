a541701c4f10a5eae666b8176c69c8ea50edd759
BratPfanneTV <pn@bptv.eu>
2018-11-04 18:28:30 +0100

    v5

---
+++ projects/CALCULATOR/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using static System.Math;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace CALCULATOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sep = ".";
+            string calculation = String.Join(" ", args).Replace(",", sep).Replace(".", sep);
+            string resultString = new DataTable().Compute(calculation, null).ToString();
+            int result = Int32.Parse(resultString);
+            Console.WriteLine(result);
+        }
+    }
+}
